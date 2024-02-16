677000f9f823b95aae134525aef2db855b4e2f22
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-02-16 15:30:57 +0200

    Implemented Extension methods for cors and IIS and configured app services and pipeline

---
+++ AnunumEmployees/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using AnunumEmployees.Extensions;
+
+namespace AnunumEmployees;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.ConfigureCors();
+        builder.Services.ConfigureIISIntegration();
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        if (app.Environment.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+        else
+            app.UseHsts();
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.All
+        });
+
+        app.UseCors("CorsPolicy");
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
