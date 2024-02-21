1ab1ea2506284c61b8a0724eedcd5a6c478d8526
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-02-21 13:53:53 +0200

    Create classlib for Entities and Repository

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,43 @@
+namespace Entities.Models;
+
+public class Company
+{
+    [Column("CompanyId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Company name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Company name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string? Address { get; set; }
+
+    public string? Country { get; set; }
+
+    public ICollection<Employee>? Employees { get; set; }
+}
+
+public class Employee
+{
+    [Column("EmployeeId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Employee name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Age is a required field.")]
+    public int Age { get; set; }
+
+    [Required(ErrorMessage = "Position is a required field.")]
+    [MaxLength(20, ErrorMessage = "Maximum length for Position is 20 characters.")]
+    public string? Position { get; set; }
+
+    [ForeignKey(nameof(Company))]
+    public Guid CompanyId { get; set; }
+    public Company? Company { get; set; }
+}
+
+
+
