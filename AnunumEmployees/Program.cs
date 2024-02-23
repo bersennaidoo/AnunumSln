e9317fb7e74671d0865aecbdc01cd7cd2c751ea9
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-02-23 16:05:09 +0200

    Implemented Service Contract, Service layer, Repository manager to abstract upstream client from implementation details

--- AnunumEmployees/Program.cs
+++ AnunumEmployees/Program.cs
@@ -19,8 +19,11 @@ public class Program
 
         builder.Services.ConfigureCors();
         builder.Services.ConfigureIISIntegration();
+        builder.Services.ConfigureSqlContext(builder.Configuration);
         builder.Services.ConfigureLoggerService();
 
+        builder.Services.ConfigureRepositoryManager();
+        builder.Services.ConfigureServiceManager();
         builder.Services.AddControllers();
 
         var app = builder.Build();
